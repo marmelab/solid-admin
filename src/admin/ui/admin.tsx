24d738b03a4a788e4eca4ca0ad23352dcb05f874
djhi <1122076+djhi@users.noreply.github.com>
2023-01-18 17:06:16 +0100

    Dirty TS

--- src/admin/ui/admin.tsx
+++ src/admin/ui/admin.tsx
@@ -2,7 +2,7 @@ import { JSX, mergeProps } from "solid-js";
 import { CoreAdmin } from "../core";
 import { Layout } from './layout';
 
-export const Admin = (props: { children: JSX.Element; layout: any }) => {
+export const Admin = (props: { children: JSX.Element; layout: any } & any) => {
     const merged = mergeProps({ layout: Layout }, props);
     return <CoreAdmin {...merged} />;
 };
